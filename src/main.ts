a2979cfcb5ef2e92950b32dab7409465c59e8552
resessh <info@resessh.net>
2021-03-06 13:54:55 +0900

    add

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
 import * as core from '@actions/core'
+import github from '@actions/github'
+
+const ctx = github.context
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const token = core.getInput('GITHUB_TOKEN', {required: true})
+    const reviewers = core.getInput('REVIEWERS').split(',')
+    const teamReviewers = core.getInput('TEAM_REVIEWERS').split(',')
 
-    core.setOutput('time', new Date().toTimeString())
+    const octokit = github.getOctokit(token, {})
+
+    if (!ctx.payload.pull_request?.number) return
+    const {data: pullRequest} = await octokit.pulls.get({
+      owner: ctx.repo.owner,
+      repo: ctx.repo.repo,
+      pull_number: ctx.payload.pull_request.number
+    })
+
+    const requestedReviewersNum = pullRequest.requested_reviewers?.length ?? 0
+    if (requestedReviewersNum > 0) return
+
+    await octokit.pulls.requestReviewers({
+      owner: ctx.repo.owner,
+      repo: ctx.repo.repo,
+      pull_number: ctx.payload.pull_request.number,
+      reviewers,
+      team_reviewers: teamReviewers
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
